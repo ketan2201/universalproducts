579d6300d0225cbd4732c4af2ebbb0fc3f5595ac
ketan <sakpalketank@gmail.com>
2023-09-20 15:30:02 +0530

    First commit

---
+++ js/sendEmail.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        const subject = `Contact Form Submission from ${name}`;
+        const body = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+
+        // Replace 'your_email@gmail.com' with your Gmail address
+        const recipient = 'universalproducts81@gmail.com';
+
+        const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+        // Open the user's default email client with the pre-filled email
+        window.location.href = mailtoLink;
+    });
+});
